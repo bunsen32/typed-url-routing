3b2982d2b44331e0ddb1ed78022512f8d6b56974
Andrew Forrest <af@cygnets.co.uk>
2012-06-21 16:16:35 +0100

    Adds helper class for creating links.
    MVC has two (very overloaded) helper methods, ActionLink and RouteLink for
    creating links. Our version, 'Link' just reuses the generation code on
    UrlPattern (which produces a string link).

---
+++ main/HtmlHelperExtensions.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HtmlExtensions.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.Web.Mvc.Html
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Web.Mvc;
+
+	/// <summary>
+	/// TODO: Update summary.
+	/// </summary>
+	public static class HtmlHelperExtensions
+	{
+		public static MvcHtmlString Link(this HtmlHelper self, string linkText, string urlString, IDictionary<string, object> htmlAttributes = null)
+		{
+			if (String.IsNullOrEmpty(linkText))
+				throw new ArgumentException("Link text not allowed to be null or empty", "linkText");
+
+			TagBuilder tagBuilder = new TagBuilder("a")
+			{
+				InnerHtml = (!String.IsNullOrEmpty(linkText)) ? HttpUtility.HtmlEncode(linkText) : String.Empty
+			};
+
+			if (htmlAttributes != null)
+			{
+				var attributeDictionary = (htmlAttributes as IDictionary<string, object>) ?? HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+				tagBuilder.MergeAttributes(attributeDictionary);
+			}
+
+			tagBuilder.MergeAttribute("href", urlString);
+			var linkString = tagBuilder.ToString(TagRenderMode.Normal);
+			return MvcHtmlString.Create(linkString);
+		}
+	}
+}
