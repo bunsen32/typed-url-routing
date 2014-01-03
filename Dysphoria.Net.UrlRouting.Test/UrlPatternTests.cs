20b592099976f19b2bf6631a3faa64399d3389ce
Andrew Forrest <af@cygnets.co.uk>
2014-01-03 22:06:34 +0000

    Adds copyright headers to test files.

--- Dysphoria.Net.UrlRouting.Test/UrlPatternTests.cs
+++ Dysphoria.Net.UrlRouting.Test/UrlPatternTests.cs
@@ -1,4 +1,14 @@
-﻿namespace Dysphoria.Net.UrlRouting.Test
+﻿// -----------------------------------------------------------------------
+// <copyright file="UrlPatternTests.cs" company="Andrew Forrest">©2013 Andrew Forrest</copyright>
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. Copy of
+// license at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES 
+// OR CONDITIONS. See License for specific permissions and limitations.
+// -----------------------------------------------------------------------
+namespace Dysphoria.Net.UrlRouting.Test
 {
 	using System;
 	using Dysphoria.Net.UrlRouting.PathComponents;
